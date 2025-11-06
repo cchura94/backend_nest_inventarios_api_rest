d7cf9cc234b0242be20fb64d2017d69da21dc8e1
cristian <cchura.cpc@gmail.com>
2025-11-05 21:40:15 -0400

    Config Base de datos + Variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), 
+    ConfigModule.forRoot({
+      envFilePath: ['.development.env', '.production.env']
+    }), 
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'postgresql',
-      database: 'bd_nest_backend',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +`${process.env.PORT}` || 5433,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'postgresql',
+      database: process.env.DATABASE_NAME || 'bd_nest_backend',
       entities: [
         __dirname + '/../**/*.entity{.ts,.js}'
       ],
