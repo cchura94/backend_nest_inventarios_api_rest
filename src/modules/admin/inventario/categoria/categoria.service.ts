870c2938c06fdc251dfde4091d32c14a49f0f5b6
cristian <cchura.cpc@gmail.com>
2025-11-11 22:26:42 -0400

    relacion entidades migrations

--- src/modules/admin/inventario/categoria/categoria.service.ts
+++ src/modules/admin/inventario/categoria/categoria.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categoria } from './entities/categoria.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriaService {
+  constructor(
+    @InjectRepository(Categoria)
+    private readonly categoriaRepository: Repository<Categoria>
+  ){
+
+  }
   create(createCategoriaDto: CreateCategoriaDto) {
     return 'This action adds a new categoria';
   }
