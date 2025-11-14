86b0a2714ce7d672e21d00fbb4aceb79a311114a
cristian <cchura.cpc@gmail.com>
2025-11-13 22:38:20 -0400

    notas compras y ventas

--- src/modules/admin/nota/nota.service.ts
+++ src/modules/admin/nota/nota.service.ts
@@ -1,19 +1,126 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateNotaDto } from './dto/create-nota.dto';
 import { UpdateNotaDto } from './dto/update-nota.dto';
+import { FiltroNotaDto } from './dto/filtro-nota.dto';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { Nota } from './entities/nota.entity';
+import { Repository } from 'typeorm';
+import { DataSource } from 'typeorm/browser';
+import { User } from '../users/entities/user.entity';
+import { ClienteProveedor } from '../cliente-proveedor/entities/cliente-proveedor.entity';
+import { Producto } from '../inventario/producto/entities/producto.entity';
+import { Almacen } from '../inventario/almacen/entities/almacen.entity';
+import { Movimiento } from './entities/movimiento.entity';
 
 @Injectable()
 export class NotaService {
-  create(createNotaDto: CreateNotaDto) {
-    return 'This action adds a new nota';
+
+  constructor(
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
+
+    @InjectRepository(Nota)
+    private notaRepo: Repository<Nota>
+  ){
+
   }
 
-  findAll() {
-    return `This action returns all nota`;
+  async create(createNotaDto: CreateNotaDto) {
+    // transacciones
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect()
+    await queryRunner.startTransaction();
+
+    try {
+      const userRepo = queryRunner.manager.getRepository(User);
+      const clienteRepo = queryRunner.manager.getRepository(ClienteProveedor);
+      const notaRepo = queryRunner.manager.getRepository(Nota);
+      const productoRepo = queryRunner.manager.getRepository(Producto);
+      const almacenRepo = queryRunner.manager.getRepository(Almacen);
+      const movimientoRepo = queryRunner.manager.getRepository(Movimiento);
+
+      const user = await userRepo.findOneBy({id: createNotaDto.user_id});
+      if(!user) throw new NotFoundException('Usuario no encontrado');
+
+      const cliente = await clienteRepo.findOneBy({id: createNotaDto.cliente_id});
+      if(!cliente) throw new NotFoundException('Cliente no encontrado');
+
+      // crear nota
+      const nota = await notaRepo.create({
+        ...createNotaDto,
+        cliente: cliente,
+        user: user
+      })
+
+      // Primero debemos guardar la nota para obtener el ID para registrar movimientos
+      await notaRepo.save(nota);
+
+      // registrar ingresos y salidas de inventario
+      // actualizar stock de inventario
+
+
+      await queryRunner.commitTransaction();      
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;      
+    } finally {
+      await queryRunner.release();
+    }
+    
+    
+  }
+
+  async findAll(filtro: FiltroNotaDto) {
+    const query = this.notaRepo.createQueryBuilder('nota')
+                    .leftJoinAndSelect('nota.user', 'user')
+                    .leftJoinAndSelect('nota.cliente', 'cliente')
+                    .leftJoinAndSelect('nota.movimientos', 'movimientos')
+                    .leftJoinAndSelect('movimientos.producto', 'producto');
+    
+    if(filtro.tipo_nota){
+      query.andWhere('nota.tipo_nota = :tipo_nota', {tipo_nota: filtro.tipo_nota});
+    }
+
+    if(filtro.estado_nota){
+      query.andWhere('nota.estado_nota = :estado_nota', {estado_nota: filtro.estado_nota});
+    }
+
+    if(filtro.desde){
+      query.andWhere('nota.fecha = :desde', {desde: filtro.desde});
+    }
+
+    if(filtro.hasta){
+      query.andWhere('nota.fecha = :hasta', {hasta: filtro.tipo_nota});
+    }
+
+    if(filtro.user_id){
+      query.andWhere('nota.user_id = :user_id', {user_id: filtro.user_id});
+    }
+
+
+    if(filtro.cliente_id){
+      query.andWhere('nota.cliente_id = :cliente_id', {cliente_id: filtro.cliente_id});
+    }
+
+    query.orderBy('nota.fecha', 'DESC');
+
+    // paginación
+    const limit = filtro.limit || 10;
+    const page = filtro.page || 1;
+
+    query.skip((page - 1) * limit).take(limit);
+
+    const [data, total] = await query.getManyAndCount();
+
+    return {data, total}
+    
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} nota`;
+    return this.notaRepo.findOne({
+      where: {id},
+      relations: ['movimientos']
+    })
   }
 
   update(id: number, updateNotaDto: UpdateNotaDto) {
